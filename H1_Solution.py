6344fcf70910de377ff3542dddccf94f12b24bab
Scott-Jia <bojia2020@gmail.com>
2021-12-07 18:18:25 -0600

    create 1st plot function and debug some typos

--- H1_Solution.py
+++ H1_Solution.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from pandas import DataFrame
 
 
-def getUniqueAscii(file: str) -> DataFrame:
+def get_unique_ascii(file: str) -> DataFrame:
     """
     Given the country data, return a pandas dataframe, with cleaned city-city_ascii mapping
     :param file: str, countries' csv data file
@@ -13,13 +13,13 @@ def getUniqueAscii(file: str) -> DataFrame:
     df = pd.read_csv(file)
     df = df.dropna()
     # print(df)
-    df = df[['city', 'city_ascii', 'lat']]
-    df = df.drop_duplicates(subset=['city'])
+    df = df[["city", "city_ascii", "lat"]]
+    df = df.drop_duplicates(subset=["city"])  # remove duplicates
     # print(df)
     return df
 
 
-def joinData(temp_data: DataFrame, ascii_data: DataFrame) -> DataFrame:
+def join_data(temp_data: DataFrame, ascii_data: DataFrame) -> DataFrame:
     """
     Given two dataframes, join them based on common column and clean the data
     :param temp_data: the cleaned temperature dataframe
@@ -27,16 +27,18 @@ def joinData(temp_data: DataFrame, ascii_data: DataFrame) -> DataFrame:
     :return: a joined and cleaned dataframe
     """
     # joined_df = temp_data.set_index('City').join(asc_data.set_index('city'))
-    joined_df = pd.merge(temp_data, ascii_data, how='outer', on='city')
-    print(joined_df)
+    # merge the two tables, based on common column
+    joined_df = pd.merge(temp_data, ascii_data, how="outer", on="city")
+    # print(joined_df)
     new_df = joined_df.drop_duplicates(
-        subset=['city', 'Year'],
-        keep='last').reset_index(drop=True)
-    print(new_df)
+        subset=["city", "Year"], keep="last"
+    ).reset_index(drop=True)
+    new_df.dropna()
+    # print(new_df)
     return new_df
 
 
-def getAvgTemp(file: str) -> DataFrame:
+def get_avg_temp(file: str) -> DataFrame:
     """
     Given a csv date with country, monthly temperature
     :param file: str, a csv data file
@@ -46,17 +48,39 @@ def getAvgTemp(file: str) -> DataFrame:
     df = df.dropna()
     # print(df)
     # add a column of year extract from date info
-    df['Year'] = pd.DatetimeIndex(df['dt']).year
-    df = df.rename(columns={'City': 'city'})
+    df["Year"] = pd.DatetimeIndex(df["dt"]).year
+    # update some columns' name
+    df = df.rename(columns={"City": "city", "AverageTemperature": "AvgTemp"})
     # group by city and year, and calculate the avg temp
-    group_df = df.groupby(['city', 'Year'])
-    mean_df = group_df['AverageTemperature'].mean()
+    group_df = df.groupby(["city", "Year"])
+    mean_df = group_df["AvgTemp"].mean()
     mean_df = mean_df.reset_index()
     # print(mean_df)
     return mean_df
 
 
-if __name__ == '__main__':
-    tem_data = getAvgTemp('h1_data/GlobalLandTemperaturesByCity.csv')
-    asc_data = getUniqueAscii('h1_data/worldcities.csv')
-    joined = joinData(tem_data, asc_data)
+def plot_temp_change(city_name: str, year: int, df: DataFrame) -> None:
+    """
+    Given information of city, start year, and pandes dataframe,
+    plot the temperature change at that city since the given start year
+    :param city_name: str, city we want to check
+    :param year: int, the start year we want to be plotted
+    :param df: pandas.dataframe, a cleaned dataframe including useful infos
+    :return: None
+    """
+    df = df[df.Year >= year]  # extract the dataframe by given info
+    city_case = df[df.city == city_name]
+    # print(city_case)
+    plt.plot(city_case.Year, city_case.AvgTemp)
+    plt.xlabel("year")
+    plt.ylabel("temperature(°C)")
+    plt.title("temperature change at {}, since {}".format(city_name, year))
+    plt.savefig("sample_output/output1.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    tem_data = get_avg_temp("h1_data/GlobalLandTemperaturesByCity.csv")
+    asc_data = get_unique_ascii("h1_data/worldcities.csv")
+    joined_data = join_data(tem_data, asc_data)
+    plot_temp_change("Miami", 1980, joined_data)

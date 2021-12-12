57914e38b9a625fc96fb338c25a6a18d746cb62a
Tiffany Kim <tiffkim007@gmail.com>
2021-12-11 19:59:00 -0600

    functions that get aggregated country data

--- H2_Solution.py
+++ H2_Solution.py
@@ -3,10 +3,6 @@ import pandas as pd
 #Hypothesis #2: Countries with ascending GDP and population emit higher CO2 per capita than
 #countries with descending GDP. Datasets needed: GDP, population, CO2 emission by countries
 
-# data analysis here ...
-
-# conclusino: based on **the info above**, the hypothesis is true/false
-
 
 def get_data(filename: str) -> pd.DataFrame:
     df = pd.read_csv(filename)
@@ -37,8 +33,40 @@ def merge_data(gdp_data: pd.DataFrame, pop_data: pd.DataFrame, co2_data: pd.Data
     ).dropna().reset_index(drop=True)
 
 
+def calculate_population_growth(grouped, country):
+    country_df = grouped.get_group(country)
+    min_year = country_df["Year"].min()
+    max_year = country_df["Year"].max()
+    if max_year == 2020:
+        max_year -= 1
+    initial = country_df.loc[country_df["Year"] == min_year, "population"].values[0]
+    final = country_df.loc[country_df["Year"] == max_year, "population"].values[0]
+    return (final - initial) * 100 / initial
+
+
+def calculate_avg_growth(grouped, country, column):
+    country_df = grouped.get_group(country)
+    return country_df[country_df["Year"] != 2020][column].mean()
+
+def get_country_data(df):
+    grouped = df.groupby("Entity")
+    countries = list()
+    pop_growths = list()
+    gdp_growths = list()
+    avg_co2 = list()
+    for country in df["Entity"].unique():
+        countries.append(country)
+        pop_growths.append(calculate_population_growth(grouped, country))
+        gdp_growths.append(calculate_avg_growth(grouped, country, "GDP growth %"))
+        avg_co2.append(calculate_avg_growth(grouped, country, "Annual CO2 emissions (per capita)"))
+    data = {"country": countries, "population growth": pop_growths, "gdp growth": gdp_growths, "avg co2": avg_co2}
+    return pd.DataFrame(data)
+
+
 if __name__ == "__main__":
     gdp_df = get_data("/Users/chanmitk/Downloads/API_NY.GDP.MKTP.KD.ZG_DS2_en_csv_v2_3358388/API_NY.GDP.MKTP.KD.ZG_DS2_en_csv_v2_3358388.csv")
     pop_df = get_data("/Users/chanmitk/Downloads/API_SP.POP.TOTL_DS2_en_csv_v2_3358390/API_SP.POP.TOTL_DS2_en_csv_v2_3358390.csv")
     co2_df = get_co2_data("/Users/chanmitk/Desktop/co2.csv")
     merged_df = merge_data(gdp_df, pop_df, co2_df)
+    country_df = get_country_data(merged_df)
+
